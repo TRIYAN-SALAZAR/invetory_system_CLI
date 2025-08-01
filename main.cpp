dcd347463331157e7e5ed554f8a35b4882a0c131
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-07-31 23:28:38 -0600

    Implement menu function and update main logic for product management options

--- main.cpp
+++ main.cpp
@@ -7,11 +7,40 @@ void updateProduct();
 void deleteProduct();
 void getListAllProducts();
 void updateStock();
+int menu();
 
 int main() {
-    std::cout << "Starting a New Personal Project For Learn";
-
+    int opt = menu();
+    switch (opt)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        std::cout << "Opcion no valida\n";
+        break;
+    }
     return 0;
+}
+
+int menu() {
+    int opt;
+    std::cout << "Que accion deseas realizar?\n";
+    std::cout << "1) Dar de alta un producto\n";
+    std::cout << "2) Buscar un producto\n";
+    std::cout << "3) Actualizar un producto\n";
+    std::cout << "4) Eliminar un producto\n";
+    std::cout << "5) Lista de todos los producto\n";
+
+    std::cin >> opt;
+    return opt;
 }
 
 void addNewProduct() {}
