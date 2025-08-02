d3a63c226cde81737b90535f82fdc1b87375af8d
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-08-01 23:13:25 -0600

    Implement addNewProduct function to handle product creation and storage

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 class Product;
+std::vector<Product> PRODUCTS;
 
 void addNewProduct();
 void searchProduct();
@@ -17,6 +18,7 @@ int main() {
     switch (opt)
     {
     case 1:
+        addNewProduct();
         break;
     case 2:
         break;
@@ -46,7 +48,38 @@ int menu() {
     return opt;
 }
 
-void addNewProduct() {}
+void addNewProduct() {
+    int code;
+    int stock;
+    float priceBought;
+    float priceSell;
+    std::string description = "";
+
+    std::cout << "Ingresa codigo: ";
+    std::cin >> code;
+    
+    std::cout << "\nIngresa la cantidad total del producto: ";
+    std::cin >> stock;
+
+    std::cout << "\nIngresa el precio de compra: ";
+    std::cin >> priceBought;
+
+    std::cout << "\nIngresa el precio de venta: ";
+    std::cin >> priceSell;
+
+    std::cout << "\nIngresa una breve descripcion: ";
+    std::cin >> description;
+
+    Product product;
+    if(description != "") {
+        product.setProduct(code, stock, priceSell, priceBought);
+    } else {
+        product.setProduct(code, stock, priceSell, priceBought, description);
+    }
+
+    PRODUCTS.push_back(product);
+}
+
 void searchProduct() {}
 void updateProduct() {}
 void deleteProduct() {}
@@ -62,14 +95,16 @@ class Product {
         std::string description;
 
     public:
-        Product(int code, int stock, float priceSell, float priceBought) {
+        Product() {}
+
+        void setProduct(int code, int stock, float priceSell, float priceBought) {
             this->code = code;
             this->stock = stock;
             this->priceSell = priceSell;
             this->priceBought = priceBought;
         }
 
-        Product(int code, int stock, float priceSell, float priceBought, std::string description) {
+        void setProduct(int code, int stock, float priceSell, float priceBought, std::string description) {
             this->code = code;
             this->stock = stock;
             this->priceSell = priceSell;
