4b18ebb1defb95f5eb891b031e7c76e0770bd734
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-08-03 21:58:06 -0600

    Refactor Product class: move implementation to product_class.hpp and clean up main.cpp

--- includes/product_class.hpp
+++ includes/product_class.hpp
@@ -1,4 +1,40 @@
 #ifndef HEADER_H_PRODUCT_CLASS
 #define HEADER_H_PRODUCT_CLASS
 
+#include <string>
+#include <iostream>
+
+class Product {
+    public:
+        int code;
+        int stock;
+        float priceBought;
+        float priceSell;
+        std::string description;
+
+        Product() {}
+
+        Product(int code, int stock, float priceSell, float priceBought, std::string description) {
+            this->code = code;
+            this->stock = stock;
+            this->priceSell = priceSell;
+            this->priceBought = priceBought;
+            if(description != "") this->description = description;
+        }
+
+        void updateProduct(int stock, float priceSell, float priceBought, std::string description) {
+            if(stock != -1) this->stock = stock;
+            if(priceSell != -1)this->priceSell = priceSell;
+            if(priceBought != -1)this->priceBought = priceBought;
+            if(description != "")this->description = description;
+        }
+
+        void showDataProduct() {
+            std::cout << "Codigo: " << this->code;
+            std::cout << "\nStock: " << this->stock;
+            std::cout << "\nPrecio de Venta: " << this->priceSell;
+            std::cout << "\nPrecio de compra: " << this->priceBought;
+            std::cout << "\nDescripcion: " << this->description;
+        }
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,7 @@
 #include <string>
 #include <vector>
 #include <limits>
-
-class Product {
-    public:
-        int code;
-        int stock;
-        float priceBought;
-        float priceSell;
-        std::string description;
-
-        Product() {}
-
-        Product(int code, int stock, float priceSell, float priceBought, std::string description) {
-            this->code = code;
-            this->stock = stock;
-            this->priceSell = priceSell;
-            this->priceBought = priceBought;
-            if(description != "") this->description = description;
-        }
-
-        void updateProduct(int stock, float priceSell, float priceBought, std::string description) {
-            if(stock != -1) this->stock = stock;
-            if(priceSell != -1)this->priceSell = priceSell;
-            if(priceBought != -1)this->priceBought = priceBought;
-            if(description != "")this->description = description;
-        }
-
-        void showDataProduct() {
-            std::cout << "Codigo: " << this->code;
-            std::cout << "\nStock: " << this->stock;
-            std::cout << "\nPrecio de Venta: " << this->priceSell;
-            std::cout << "\nPrecio de compra: " << this->priceBought;
-            std::cout << "\nDescripcion: " << this->description;
-        }
-};
+#include <product_class.hpp>
 
 std::vector<Product> PRODUCTS;
 
