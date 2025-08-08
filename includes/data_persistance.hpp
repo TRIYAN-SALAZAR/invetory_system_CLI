14d5af9258a2ab32890acdb56fc77a7f1cb3144b
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-08-07 21:37:37 -0600

    Add data persistence functions: readAndExtractTheData, writeNewData, deleteRow, and updateRow

---
+++ includes/data_persistance.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+void readAndExtractTheData();
+void writeNewData();
+void deleteRow();
+void updateRow();
---
+++ src/data_persistance.cpp
@@ -0,0 +1,6 @@
+#include <data_persistance.hpp>
+
+void readAndExtractTheData() {}
+void writeNewData() {}
+void deleteRow() {}
+void updateRow() {}
